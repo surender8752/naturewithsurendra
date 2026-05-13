6391b97ad1290eeb793663e5e61a21e9a018bc4d
surender <surenderthakur40437@gmail.com>
2026-05-13 11:10:06 +0530

    k

--- src/App.jsx
+++ src/App.jsx
@@ -136,15 +136,7 @@ const toggleMusic = () => {
 
 </div>
 
-  {/* BIRDS */}
-
-<div className="birds">
-
-  <div className="bird bird1">🕊️</div>
-  <div className="bird bird2">🕊️</div>
-  <div className="bird bird3">🕊️</div>
-
-</div>      
+      
 
 
         
