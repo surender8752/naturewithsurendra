5f97bb9e13298c243af7fa8104f2eb8dda84312b
surender <surenderthakur40437@gmail.com>
2026-05-13 10:37:53 +0530

    k

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ const toggleMusic = () => {
   />
 
         {/* DARK OVERLAY */}
-        <div className="absolute inset-0 bg-black/50"></div>
+       <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-black/30 to-black/80"></div>
 
         {/* CLOUDS */}
 <div className="cloud cloud1"></div>
